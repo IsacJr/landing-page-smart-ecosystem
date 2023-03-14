f0d0ed90aa58dc60d20c666c5852e11765c614f2
Isac <isac.aniceto@hotmail.com>
2023-03-13 23:22:47 -0300

    remove unecessary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { useContext } from "react";
 import "./App.css";
-import ThemeProvider, { ThemeContext } from "./context/ThemeContext";
+import { ThemeContext } from "./context/ThemeContext";
 import Home from "./pages/Home";
 
 function App() {
   const { isDarkMode } = useContext(ThemeContext);
-  console.log(isDarkMode);
 
   return (
     <div className={"app-container " + (isDarkMode ? "dark-theme" : "")}>
